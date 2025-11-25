2a4b5ab4dc2db7b13df9e9262f2e3d55fa130cae
RajaShahzaibNaseer <shahzaib.naseer.work2@gmail.com>
2025-11-25 17:36:15 +0500

    made game functional

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #to-do list
 # make the menu appear to highlight choices (done)
-# add choice functionality 
-# add number generator
+# add choice functionality (done) 
+# add number generator (done)
 # add game loop
 
 import random
@@ -12,7 +12,6 @@ def menu():
     print("I'm thinking of a number between 1 and 100")
     print("\nPlease select the difficulty level")
     print("1.Easy (10 chances) \n 2. Medium (5 chances) \n 3.Hard (3 chances)")
-    print("\n Enter your choice: ")
 
 def main():
     number_to_guess = random.randint(0, 100)
@@ -21,23 +20,27 @@ def main():
     while game_loop:
         menu()
         choice = input("Enter your choice: ")
+        print(choice)
         if choice == 1:
             difficulty = 10
-        elif choice = 2:
+        elif choice == 2:
             difficulty = 5
         else:
             difficulty = 3
-
+        
+        print(difficulty)
         while difficulty != 0:
             guess = input("enter your guess")
             if guess == number_to_guess:
                 game_loop = False
                 difficulty = 0
             else:
-                print("You guessed wrong!")
+                compare_numbers = lambda x : f"number is greater then {x}" if int(x) > number_to_guess else f"number is less then {x}"
+                print(f"You guessed wrong!{compare_numbers(guess)}")
                 print(f"you have {difficulty} guesses remaining")
                 difficulty -= 1
 
-
+if __name__ == "__main__":
+    main()
 
 
