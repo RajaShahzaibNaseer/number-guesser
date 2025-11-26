985bbdc2d65fe44ade328fdb728c48e28e5400fb
RajaShahzaibNaseer <shahzaib.naseer.work2@gmail.com>
2025-11-26 14:20:36 +0500

    fixed number comparison

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 # make the menu appear to highlight choices (done)
 # add choice functionality (done) 
 # add number generator (done)
-# add game loop
+# add game loop 
+# fix difficulty not being chosen correctly
+# fix menu layout
+# add game end state
+# add game quit option
+# fix compare_numbers not working
+#
 
 import random
 
@@ -11,10 +17,11 @@ def menu():
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100")
     print("\nPlease select the difficulty level")
-    print("1.Easy (10 chances) \n 2. Medium (5 chances) \n 3.Hard (3 chances)")
+    print("1.Easy (10 chances) \n 2.Medium (5 chances) \n3.Hard (3 chances)")
 
 def main():
     number_to_guess = random.randint(0, 100)
+    print(number_to_guess)
     game_loop = True
     difficulty = 0
     while game_loop:
@@ -27,7 +34,6 @@ def main():
             difficulty = 5
         else:
             difficulty = 3
-        
         print(difficulty)
         while difficulty != 0:
             guess = input("enter your guess")
@@ -35,7 +41,7 @@ def main():
                 game_loop = False
                 difficulty = 0
             else:
-                compare_numbers = lambda x : f"number is greater then {x}" if int(x) > number_to_guess else f"number is less then {x}"
+                compare_numbers = lambda x : f"number is greater then {x}" if int(x) < number_to_guess else f"number is less then {x}"
                 print(f"You guessed wrong!{compare_numbers(guess)}")
                 print(f"you have {difficulty} guesses remaining")
                 difficulty -= 1
